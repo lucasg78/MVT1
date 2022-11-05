be7379810935908d572c32488f0a59289debcc8a
Lucas Gallo <lucasgallo@gmail.com>
2022-11-05 16:50:35 -0300

    Second commit

--- MVT1/AppMVT1/models.py
+++ MVT1/AppMVT1/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Familiares(models.Model):
-    
+class Familiares(models.Model):   
     nombre = models.CharField(max_length=50)
     edad = models.IntegerField()
     nacimiento = models.DateField()
